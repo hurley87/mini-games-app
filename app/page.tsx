990f1db15494d195f228635c16c2ea2fa0499e66
David Hurley <dhurls99@gmail.com>
2025-05-16 23:29:24 -0400

    auto show the frame button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { useMiniKit, useAddFrame } from '@coinbase/onchainkit/minikit';
-import { useEffect, useMemo, useState, useCallback } from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { useAccount, useConnect } from 'wagmi';
 
 export default function App() {
   const { setFrameReady, isFrameReady, context } = useMiniKit();
   const { address } = useAccount();
-  const [frameAdded, setFrameAdded] = useState(false);
   const { isConnected } = useAccount();
   const { connectors, connect } = useConnect();
 
@@ -22,6 +21,14 @@ export default function App() {
       setFrameReady();
     }
   }, [setFrameReady, isFrameReady]);
+
+  useEffect(() => {
+    if (!context?.client?.added) {
+      (async () => {
+        await addFrame();
+      })();
+    }
+  }, [context]);
 
   useEffect(() => {
     const saveUser = async () => {
@@ -57,41 +64,9 @@ export default function App() {
     saveUser();
   }, [context]);
 
-  const handleAddFrame = useCallback(async () => {
-    const frameAdded = await addFrame();
-    setFrameAdded(Boolean(frameAdded));
-  }, [addFrame]);
-
-  const saveFrameButton = useMemo(() => {
-    if (context && !context.client.added) {
-      return (
-        <button
-          onClick={handleAddFrame}
-          className="text-[var(--app-accent)] p-4"
-        >
-          Save Frame
-        </button>
-      );
-    }
-
-    if (frameAdded) {
-      return (
-        <div className="flex items-center space-x-1 text-sm font-medium text-[#0052FF] animate-fade-out">
-          <span>Saved</span>
-        </div>
-      );
-    }
-
-    return null;
-  }, [context, frameAdded, handleAddFrame]);
-
-  console.log('context', context);
-  // const userId = context?.user?.fid?.toString() || "";
-
   return (
     <div className="flex flex-col min-h-screen font-sans text-[var(--app-foreground)] mini-app-theme from-[var(--app-background)] to-[var(--app-gray)]">
       Mini Games
-      {saveFrameButton}
       <Link href="/info/d87ab2c1-94ca-49e1-8bd7-756bf848f2a6">Play Game</Link>
       {!isConnected && (
         <button onClick={() => connect({ connector: connectors[0] })}>
