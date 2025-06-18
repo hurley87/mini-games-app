534042ab7df235fe6e0214fba1576bf17336f592
David Hurley <dhurls99@gmail.com>
2025-06-18 18:21:21 +0200

    remove welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from './components/header';
 import { CoinsList } from './components/coins-list';
-import { WelcomeDialog } from './components/welcome-dialog';
 import { BottomNav } from './components/bottom-nav';
 
 export default function Page() {
@@ -8,7 +7,6 @@ export default function Page() {
     <div className="max-w-lg mx-auto min-h-screen flex flex-col bg-gradient-to-b from-black via-zinc-900 to-black text-white pb-20">
       <Header />
       <CoinsList />
-      <WelcomeDialog />
       <BottomNav />
     </div>
   );
